7eedb1cc4b9e6ca73bd7941365cb26b36be33b5f
Alejandro Torres <atorresalcala.me@gmail.com>
2021-07-23 10:25:08 +0200

    Initial commit

---
+++ ionic-react-theme/src/App.tsx
@@ -0,0 +1,26 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
+
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import './assets/styles/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
