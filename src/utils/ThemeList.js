105a130e0272d84c782c5e54e98b92f05be9efb3
Alejandro Torres <atorresalcala.me@gmail.com>
2021-07-26 09:44:33 +0200

    feat(util): add list colors

---
+++ src/utils/ThemeList.js
@@ -0,0 +1,84 @@
+export const themeList = [
+	// Light Colors
+	{
+		code: '#8739fa',
+		classname: 'light-purple',
+	},
+	{
+		code: '#d763cd',
+		classname: 'light-rose',
+	},
+	{
+		code: '#4ead68',
+		classname: 'light-green',
+	},
+	{
+		code: '#888464',
+		classname: 'light-golden',
+	},
+	{
+		code: '#3986fa',
+		classname: 'light-blue',
+	},
+	{
+		code: '#e23655',
+		classname: 'light-red',
+	},
+	{
+		code: '#b1767f',
+		classname: 'light-meat',
+	},
+	{
+		code: '#f85b38',
+		classname: 'light-orange',
+	},
+	{
+		code: '#2badad',
+		classname: 'light-cyan',
+	},
+	{
+		code: '#9496a2',
+		classname: 'light-gray',
+	},
+	// Dark Colors
+	{
+		code: 'hsl(264, 75%, 60%)',
+		classname: 'dark-purple',
+	},
+	{
+		code: 'hsl(305, 39%, 62%)',
+		classname: 'dark-rose',
+	},
+	{
+		code: 'hsl(136, 28%, 49%)',
+		classname: 'dark-green',
+	},
+	{
+		code: 'hsl(53, 15%, 46%)',
+		classname: 'dark-golden',
+	},
+	{
+		code: 'hsl(216, 75%, 60%)',
+		classname: 'dark-blue',
+	},
+	{
+		code: 'hsl(349, 55%, 55%)',
+		classname: 'dark-red',
+	},
+	{
+		code: 'hsl(351, 27%, 58%)',
+		classname: 'dark-meat',
+	},
+	{
+		code: 'hsl(11, 73%, 60%)',
+		classname: 'dark-orange',
+	},
+	{
+		code: 'hsl(180, 50%, 42%)',
+		classname: 'dark-cyan',
+	},
+	{
+		code: 'hsl(231, 7%, 61%)',
+		classname: 'dark-gray',
+	},
+];
